fd2589187a39f0413daa5f932066218f53d1a866
elnuraamantur22 <elnura.amantur@gmail.com>
2024-10-30 23:41:40 +0600

    create repositories

---
+++ src/main/java/com/example/testTaskMega/repository/WorkerRepository.java
@@ -0,0 +1,15 @@
+package com.example.testTaskMega.repository;
+
+import com.example.testTaskMega.model.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, UUID> {
+
+    Optional<Worker> findById(UUID id);
+}
