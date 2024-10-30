9eab99d5067738b6204a7fb63d5fac90e6ac076b
elnuraamantur22 <elnura.amantur@gmail.com>
2024-10-31 02:38:29 +0600

    realize logic of sending email

---
+++ src/main/java/com/example/testTaskMega/service/impl/MailService.java
@@ -0,0 +1,24 @@
+package com.example.testTaskMega.service.impl;
+
+import com.example.testTaskMega.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
