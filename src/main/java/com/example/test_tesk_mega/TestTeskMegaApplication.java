bb14a2f4951325c3d3a280a7844334e2fbe541b7
elnuraamantur22 <elnura.amantur@gmail.com>
2024-10-30 16:34:08 +0600

    initial commit

---
+++ src/main/java/com/example/test_tesk_mega/TestTeskMegaApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_tesk_mega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTeskMegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTeskMegaApplication.class, args);
+	}
+
+}
